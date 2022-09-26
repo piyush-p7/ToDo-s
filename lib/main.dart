3670379f0dae201b5a462c953192cef3e02c7663
Piyush <piyushpandita151@gmail.com>
2022-09-26 18:42:28 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   List<Widget> todosList = <Widget>[
-    ToDoWidget("Go College", "At 10 A.M be in College for lab", DateTime.now()),
+    ToDoWidget(
+      "Make Bed",
+      "Remeber to tuck in the sheets",
+      DateTime.now(),
+    ),
     Divider(),
-    ToDoWidget("Back Hostel", "At 4 P.M way back to hostel", DateTime.now()),
+    ToDoWidget(
+      "Walk Dog",
+      "Remember to pick up milk",
+      DateTime.now(),
+    ),
     Divider(),
-    ToDoWidget("Go Study", "ADS", DateTime.now()),
+    ToDoWidget(
+      "Vaccum",
+      "rememeber to get behind the furniture",
+      DateTime.now(),
+    ),
     Divider(),
-    ToDoWidget("Rest", "Take rest for sometime", DateTime.now()),
-    Divider(),
-    ToDoWidget("Start study again", "ADS", DateTime.now()),
-    Divider(),
-    ToDoWidget("Sleep", "make bed", DateTime.now()),
+    ToDoWidget(
+      "Play Piano",
+      "Let's get that music Learned",
+      DateTime.now(),
+    )
   ];
+
   @override
   Widget build(BuildContext context) {
-    var navigationBarTap = (int index) {
+    var onNavigationBarTap = (int index) {
       print(index);
     };
 
-    var navigationBar = BottomNavigationBar(
-      onTap: navigationBarTap,
+    var bottomNavigationBar = BottomNavigationBar(
+      onTap: onNavigationBarTap,
       items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add ToDo"),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Todos"),
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add Todos")
       ],
     );
+
     return MaterialApp(
-      home: Scaffold(
-        bottomNavigationBar: navigationBar,
-        appBar: AppBar(
-          title: Text(
-            "To-Do App",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        body: ListView(
-          children: <Widget>[
-            Container(child: Column(children: this.todosList))
-          ],
-        ),
-      ),
-    );
+        home: Scaffold(
+            bottomNavigationBar: bottomNavigationBar,
+            appBar: AppBar(title: Text("To-Do List")),
+            body: ListView(
+              children: <Widget>[
+                Container(child: Column(children: this.todosList))
+              ],
+            )));
   }
 }
 
@@ -72,6 +77,13 @@ class ToDoState extends State<ToDoWidget> {
 
   @override
   Widget build(BuildContext context) {
+    String dateString = "Created " +
+        this.date.day.toString() +
+        "/" +
+        this.date.month.toString() +
+        "/" +
+        this.date.year.toString();
+
     var message = Expanded(
         child: SingleChildScrollView(
             child: Padding(
@@ -81,24 +93,21 @@ class ToDoState extends State<ToDoWidget> {
                   style: TextStyle(fontSize: 20.0),
                 ))));
 
-    String dateString = "Created " +
-        this.date.day.toString() +
-        "/" +
-        this.date.month.toString() +
-        "/" +
-        this.date.year.toString();
-
     var titleController = TextEditingController(text: this.title);
+
     var textTitleFieldDecoration = InputDecoration(border: InputBorder.none);
+
     var textTitleFieldStyle = TextStyle(
         fontSize: 27.0, fontWeight: FontWeight.bold, color: Colors.black);
+
     var title = TextField(
       controller: titleController,
       decoration: textTitleFieldDecoration,
       style: textTitleFieldStyle,
     );
 
-    var dateCreated = Text(dateString);
+    var dateCreated =
+        Padding(padding: EdgeInsets.only(top: 10.0), child: Text(dateString));
 
     var todoCard = Column(
       children: <Widget>[title, message, dateCreated],
@@ -106,12 +115,11 @@ class ToDoState extends State<ToDoWidget> {
     );
 
     return Container(
-      width: 400.0,
-      height: 200.0,
-      alignment: Alignment.centerLeft,
-      constraints: BoxConstraints(maxHeight: 150, maxWidth: 1000.0),
-      padding: EdgeInsets.all(19.0),
-      child: todoCard,
-    );
+        width: 400.0,
+        height: 200.0,
+        alignment: Alignment.centerLeft,
+        constraints: BoxConstraints(maxHeight: 200.0, maxWidth: 1000.0),
+        padding: EdgeInsets.all(19.0),
+        child: todoCard);
   }
 }
