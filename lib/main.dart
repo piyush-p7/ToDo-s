26381369e79991010d40e975cf09b0984012fe16
Piyush Pandita <96694286+piyush-p7@users.noreply.github.com>
2023-04-21 11:02:23 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,105 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:taskist/ui/page_done.dart';
+import 'package:taskist/ui/page_settings.dart';
+import 'package:taskist/ui/page_task.dart';
 
-void main() {
-  runApp(MyApp());
+Future<Null> main() async {
+  _currentUser = await _signInAnonymously();
+
+  runApp(new TaskistApp());
+}
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+FirebaseUser _currentUser;
+
+Future<FirebaseUser> _signInAnonymously() async {
+  final user = await _auth.signInAnonymously();
+  return user;
 }
 
-class MyApp extends StatelessWidget {
-  List<Widget> todosList = <Widget>[
-    ToDoWidget("Go College", "At 10 A.M be in College for lab", DateTime.now()),
-    Divider(),
-    ToDoWidget("Back Hostel", "At 4 P.M way back to hostel", DateTime.now()),
-    Divider(),
-    ToDoWidget("Go Study", "ADS", DateTime.now()),
-    Divider(),
-    ToDoWidget("Rest", "Take rest for sometime", DateTime.now()),
-    Divider(),
-    ToDoWidget("Start study again", "ADS", DateTime.now()),
-    Divider(),
-    ToDoWidget("Sleep", "make bed", DateTime.now()),
-  ];
+class HomePage extends StatefulWidget {
+  final FirebaseUser user;
+
+  HomePage({Key key, this.user}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class TaskistApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var navigationBarTap = (int index) {
-      print(index);
-    };
-
-    var navigationBar = BottomNavigationBar(
-      onTap: navigationBarTap,
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add ToDo"),
-      ],
-    );
     return MaterialApp(
-      home: Scaffold(
-        bottomNavigationBar: navigationBar,
-        appBar: AppBar(
-          title: Text(
-            "To-Do App",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        body: ListView(
-          children: <Widget>[
-            Container(child: Column(children: this.todosList))
-          ],
-        ),
+      debugShowCheckedModeBanner: false,
+      title: "Taskist",
+      home: HomePage(
+        user: _currentUser,
       ),
+      theme: new ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
 
-class ToDoWidget extends StatefulWidget {
-  final String title;
-  final String message;
-  final DateTime date;
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  int _currentIndex = 1;
 
-  ToDoWidget(this.title, this.message, this.date);
-
-  @override
-  State<StatefulWidget> createState() {
-    return ToDoState(this.title, this.message, this.date);
-  }
-}
-
-class ToDoState extends State<ToDoWidget> {
-  String title;
-  String message;
-  DateTime date;
-
-  ToDoState(this.title, this.message, this.date);
+  final List<Widget> _children = [
+    DonePage(
+      user: _currentUser,
+    ),
+    TaskPage(
+      user: _currentUser,
+    ),
+    SettingsPage(
+      user: _currentUser,
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
-    var message = Expanded(
-        child: SingleChildScrollView(
-            child: Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: Text(
-                  this.message,
-                  style: TextStyle(fontSize: 20.0),
-                ))));
-
-    String dateString = "Created " +
-        this.date.day.toString() +
-        "/" +
-        this.date.month.toString() +
-        "/" +
-        this.date.year.toString();
-
-    var titleController = TextEditingController(text: this.title);
-    var textTitleFieldDecoration = InputDecoration(border: InputBorder.none);
-    var textTitleFieldStyle = TextStyle(
-        fontSize: 27.0, fontWeight: FontWeight.bold, color: Colors.black);
-    var title = TextField(
-      controller: titleController,
-      decoration: textTitleFieldDecoration,
-      style: textTitleFieldStyle,
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        fixedColor: Colors.deepPurple,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: new Icon(FontAwesomeIcons.calendarCheck),
+              title: new Text("")),
+          BottomNavigationBarItem(
+              icon: new Icon(FontAwesomeIcons.calendar), title: new Text("")),
+          BottomNavigationBarItem(
+              icon: new Icon(FontAwesomeIcons.slidersH), title: new Text(""))
+        ],
+      ),
+      body: _children[_currentIndex],
     );
+  }
 
-    var dateCreated = Text(dateString);
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
-    var todoCard = Column(
-      children: <Widget>[title, message, dateCreated],
-      crossAxisAlignment: CrossAxisAlignment.start,
-    );
+  @override
+  void initState() {
+    super.initState();
 
-    return Container(
-      width: 400.0,
-      height: 200.0,
-      alignment: Alignment.centerLeft,
-      constraints: BoxConstraints(maxHeight: 150, maxWidth: 1000.0),
-      padding: EdgeInsets.all(19.0),
-      child: todoCard,
-    );
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
